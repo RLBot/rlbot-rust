d481ab14b6278f78fd761b6c163ead7642599816
John Simon <john@whatisaph.one>
2018-10-02 02:20:57 -0400

    Placate rustfmt

--- src/rlbot_generated.rs
+++ src/rlbot_generated.rs
@@ -3874,7 +3874,8 @@ pub mod rlbot {
                     .get::<QuickChatSelection>(
                         QuickChat::VT_QUICKCHATSELECTION,
                         Some(QuickChatSelection::Information_IGotIt),
-                    ).unwrap()
+                    )
+                    .unwrap()
             }
             /// The index of the player that sent the quick chat
             #[inline]
