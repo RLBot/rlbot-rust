c5e29241a7bdcf93581a424d0191ff7a2451fac7
John Simon <john@whatisaph.one>
2018-10-10 15:34:30 -0400

    Add a test for Physicist

---
+++ tests/physicist_happy.rs
@@ -0,0 +1,28 @@
+#![cfg(windows)]
+#![cfg_attr(feature = "strict", deny(warnings))]
+
+extern crate rlbot;
+extern crate winapi;
+extern crate winproc;
+
+use std::error::Error;
+
+mod common;
+
+#[test]
+fn integration_physicist_happy() -> Result<(), Box<Error>> {
+    common::with_rocket_league(|| {
+        let rlbot = rlbot::init()?;
+
+        rlbot.start_match(rlbot::ffi::MatchSettings::simple_1v1("Hero", "Villain"))?;
+
+        let mut physicist = rlbot.physicist();
+        let first = physicist.next_flat()?;
+        let second = physicist.next_flat()?;
+
+        let first_frame = first.ball().unwrap().state().unwrap().frame();
+        let second_frame = second.ball().unwrap().state().unwrap().frame();
+        assert!(second_frame > first_frame);
+        Ok(())
+    })
+}
