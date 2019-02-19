313ce44fd43df8c949b4706bf40a6c72b38a12e4
John Simon <john@whatisaph.one>
2019-02-19 02:40:47 -0500

    Update to non-deprecated method

--- tests/player_input.rs
+++ tests/player_input.rs
@@ -8,7 +8,6 @@ use std::{error::Error, thread, time::Duration};
 mod common;
 
 #[test]
-#[allow(deprecated)]
 fn integration_player_input() -> Result<(), Box<dyn Error>> {
     common::with_rocket_league(|| {
         let rlbot = rlbot::init()?;
@@ -17,7 +16,7 @@ fn integration_player_input() -> Result<(), Box<dyn Error>> {
         rlbot.wait_for_match_start()?;
 
         let mut packeteer = rlbot.packeteer();
-        let start = packeteer.next_ffi()?;
+        let start = packeteer.next()?;
 
         let input = rlbot::ControllerState {
             throttle: 1.0,
@@ -26,11 +25,11 @@ fn integration_player_input() -> Result<(), Box<dyn Error>> {
         rlbot.update_player_input(0, &input)?;
 
         thread::sleep(Duration::from_secs(1));
-        let end = packeteer.next_ffi()?;
+        let end = packeteer.next()?;
 
         // The car is facing the Y direction. It should be moving forward.
-        assert!(end.GameCars[0].Physics.Location.Y > start.GameCars[0].Physics.Location.Y + 500.0);
-        assert!(end.GameCars[0].Physics.Velocity.Y > 500.0);
+        assert!(end.players[0].physics.location.y > start.players[0].physics.location.y + 500.0);
+        assert!(end.players[0].physics.velocity.y > 500.0);
         Ok(())
     })
 }
