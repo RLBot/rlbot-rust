4b034ada7921a3f645ff75bd22fb2947e1047c26
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2019-06-18 13:28:20 -0700

    Update and rename example.bat to run-example.bat

--- example.bat
+++
@@ -1,1 +0,0 @@
-@cargo build --example bot && python -c "from rlbot import runner; runner.main()"
---
+++ run-example.bat
@@ -0,0 +1,1 @@
+@cargo build --example bot && pip install rlbot && python -c "from rlbot import runner; runner.main()"
