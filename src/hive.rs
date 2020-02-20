dfde3f88fce28b12f02193700f5957705f873e67
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-02-20 14:56:05 +0100

    Added some documentation

--- src/hive.rs
+++ src/hive.rs
@@ -1,28 +1,50 @@
 use crate::{
-    game::{GameTickPacket, ControllerState},
-    init_with_options, InitOptions
+    game::{ControllerState, GameTickPacket},
+    init_with_options, InitOptions,
 };
-use std::{env, error::Error, path::PathBuf, cmp::Ordering};
+use std::{cmp::Ordering, env, error::Error, path::PathBuf};
 
 // Most of this is basically a copy of what is in framework.rs,
 // just adapted for hivemind.
 
-// Use this like you would use the Bot trait
+/// Your hivemind bot should implement this trait.
+/// Use this like you would use the Bot trait.
+///
+/// The drone_indices are indices of the bots you control.
 pub trait Hivemind {
+    /// This method is called after your hivemind has been passed into
+    /// `run_hive()`. It gives you the a vector of bot indices which
+    /// correspond to those which you control.
     fn set_drone_indices(&mut self, indices: Vec<usize>);
+
+    /// This is called whenever there is a new game state. Your cars will be
+    /// controlled according to the inputs you return.
+    ///
+    /// The first input will go to the first index in `drone_indices`, the
+    /// second to the second, etc.
     fn tick(&mut self, packet: &GameTickPacket) -> Vec<ControllerState>;
 }
 
+/// Runs your hivemind bot. Pass in your struct which implements the Hivemind
+/// trait.
+///
+/// Calls `tick()` on your hivemind and expects a Vec<ControllerState>. The
+/// first element corresponds to the first index in `drone_indices`, the second
+/// to the second, etc.
 pub fn run_hive<H: Hivemind>(mut hive: H) -> Result<(), Box<dyn Error>> {
     let args = parse_framework_args()
-    .map_err(|_| Box::<dyn Error>::from("could not parse framework arguments"))?
-    .ok_or_else(|| Box::<dyn Error>::from("not launched by framework"))?;
+        .map_err(|_| Box::<dyn Error>::from("could not parse framework arguments"))?
+        .ok_or_else(|| Box::<dyn Error>::from("not launched by framework"))?;
 
     let drone_indices = args.drone_indices.to_vec();
     let rlbot = init_with_options(args.into())?;
-    
-    // Create a Vec<usize> copy of the original Vec<i32>
-    let usize_drone_indices: Vec<usize> = drone_indices.to_vec().into_iter().map(|element| element as usize).collect();
+
+    // Create a Vec<usize> copy of the original Vec<i32>.
+    let usize_drone_indices: Vec<usize> = drone_indices
+        .to_vec()
+        .into_iter()
+        .map(|element| element as usize)
+        .collect();
     hive.set_drone_indices(usize_drone_indices);
 
     let mut packets = rlbot.packeteer();
@@ -36,23 +58,24 @@ pub fn run_hive<H: Hivemind>(mut hive: H) -> Result<(), Box<dyn Error>> {
                     // first input corresponds to first index in drone_indices, etc.
                     let drone = drone_indices[i];
                     let input = &inputs[i];
-                    rlbot.update_player_input(drone, input);
+                    rlbot.update_player_input(drone, input)?;
                 }
-            },
+            }
             Ordering::Less => return Err(Box::<dyn Error>::from("too few inputs for drones")),
-            Ordering::Greater => return Err(Box::<dyn Error>::from("too many inputs for drones"))
+            Ordering::Greater => return Err(Box::<dyn Error>::from("too many inputs for drones")),
         }
     }
 }
 
+// Same as in framework.rs except it's for a hivemind.
 fn parse_framework_args() -> Result<Option<HiveFrameworkArgs>, ()> {
     parse_framework_command_line(env::args().skip(1))
 }
 
+// Same as in framework.rs except it's for a hivemind.
 fn parse_framework_command_line(
     mut args: impl Iterator<Item = String>,
 ) -> Result<Option<HiveFrameworkArgs>, ()> {
-
     if args.next().as_ref().map(|s| &s[..]) != Some("--rlbot-version") {
         return Ok(None); // not launched by the framework
     }
@@ -72,7 +95,6 @@ fn parse_framework_command_line(
         let index: i32 = arg.parse().map_err(|_| ())?;
         drone_indices.push(index);
     }
-    
 
     Ok(Some(HiveFrameworkArgs {
         rlbot_version,
@@ -82,22 +104,22 @@ fn parse_framework_command_line(
     }))
 }
 
-// The arguments passed by the RLBot framework for a hivemind.
+/// The arguments passed by the RLBot framework for a hivemind.
 struct HiveFrameworkArgs {
-    // The version of the RLBot framework used to launch the app. This is the
-    // same as the version shown when you run this Python code:
-    //
-    // ```python
-    // import rlbot
-    // print(rlbot.__version__)
-    // ```
+    /// The version of the RLBot framework used to launch the app. This is the
+    /// same as the version shown when you run this Python code:
+    ///
+    /// ```python
+    /// import rlbot
+    /// print(rlbot.__version__)
+    /// ```
     pub rlbot_version: String,
 
-    // The directory containing `RLBot_Core_Interface.dll` and
-    // `RLBot_Injector.exe`.
+    /// The directory containing `RLBot_Core_Interface.dll` and
+    /// `RLBot_Injector.exe`.
     pub rlbot_dll_directory: PathBuf,
 
-    // The indices of the bots you're controlling
+    /// The indices of the bots you are controlling.
     pub drone_indices: Vec<i32>,
 
     _non_exhaustive: (),
@@ -108,3 +130,5 @@ impl From<HiveFrameworkArgs> for InitOptions {
         Self::new().rlbot_dll_directory(args.rlbot_dll_directory)
     }
 }
+
+// TODO Tests?
