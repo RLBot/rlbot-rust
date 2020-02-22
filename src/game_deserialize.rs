ffbbf0c1f55aa9e04d34d8f348b7738ed1eaebc2
Nathan Caldwell <saintdev@gmail.com>
2020-02-22 14:46:15 -0700

    Remove unused deserialize_game_tick_packet()

--- src/game_deserialize.rs
+++ src/game_deserialize.rs
@@ -22,30 +22,6 @@ impl From<flat::GameTickPacket<'_>> for GameTickPacket {
     }
 }
 
-#[allow(unused)]
-#[deprecated(note = "Use The From trait implementation instead")]
-pub fn deserialize_game_tick_packet(packet: flat::GameTickPacket<'_>) -> GameTickPacket {
-    GameTickPacket {
-        players: flat_vector_iter(packet.players().unwrap())
-            .map(deserialize_player_info)
-            .collect(),
-        boost_pad_states: flat_vector_iter(packet.boostPadStates().unwrap())
-            .map(deserialize_boost_pad_state)
-            .collect(),
-        ball: packet.ball().map(deserialize_ball_info),
-        game_info: deserialize_game_info(packet.gameInfo().unwrap()),
-        tile_information: packet.tileInformation().map(|ti| {
-            flat_vector_iter(ti)
-                .map(deserialize_dropshot_tile)
-                .collect()
-        }),
-        teams: flat_vector_iter(packet.teams().unwrap())
-            .map(deserialize_team_info)
-            .collect(),
-        _non_exhaustive: (),
-    }
-}
-
 impl From<flat::PlayerInfo<'_>> for PlayerInfo {
     fn from(info: flat::PlayerInfo<'_>) -> Self {
         Self {
@@ -65,23 +41,6 @@ impl From<flat::PlayerInfo<'_>> for PlayerInfo {
     }
 }
 
-fn deserialize_player_info(info: flat::PlayerInfo<'_>) -> PlayerInfo {
-    PlayerInfo {
-        physics: deserialize_physics(info.physics().unwrap()),
-        score_info: deserialize_score_info(info.scoreInfo().unwrap()),
-        is_demolished: info.isDemolished(),
-        has_wheel_contact: info.hasWheelContact(),
-        is_supersonic: info.isSupersonic(),
-        is_bot: info.isBot(),
-        jumped: info.jumped(),
-        double_jumped: info.doubleJumped(),
-        name: info.name().unwrap().to_string(),
-        team: info.team(),
-        boost: info.boost(),
-        _non_exhaustive: (),
-    }
-}
-
 impl From<flat::BoostPadState<'_>> for BoostPadState {
     fn from(state: flat::BoostPadState<'_>) -> Self {
         Self {
@@ -89,14 +48,6 @@ impl From<flat::BoostPadState<'_>> for BoostPadState {
             timer: state.timer(),
             _non_exhaustive: (),
         }
-    }
-}
-
-fn deserialize_boost_pad_state(state: flat::BoostPadState<'_>) -> BoostPadState {
-    BoostPadState {
-        is_active: state.isActive(),
-        timer: state.timer(),
-        _non_exhaustive: (),
     }
 }
 
@@ -108,15 +59,6 @@ impl From<flat::BallInfo<'_>> for BallInfo {
             dropshot_info: info.dropShotInfo().map(DropshotBallInfo::from),
             _non_exhaustive: (),
         }
-    }
-}
-
-fn deserialize_ball_info(info: flat::BallInfo<'_>) -> BallInfo {
-    BallInfo {
-        physics: deserialize_physics(info.physics().unwrap()),
-        latest_touch: info.latestTouch().map(deserialize_touch),
-        dropshot_info: info.dropShotInfo().map(deserialize_dropshot_ball_info),
-        _non_exhaustive: (),
     }
 }
 
@@ -132,16 +74,6 @@ impl From<flat::Physics<'_>> for Physics {
     }
 }
 
-fn deserialize_physics(physics: flat::Physics<'_>) -> Physics {
-    Physics {
-        location: deserialize_vector3(physics.location().unwrap()),
-        rotation: deserialize_rotator(physics.rotation().unwrap()),
-        velocity: deserialize_vector3(physics.velocity().unwrap()),
-        angular_velocity: deserialize_vector3(physics.angularVelocity().unwrap()),
-        _non_exhaustive: (),
-    }
-}
-
 impl From<&flat::Vector3> for Vector3 {
     fn from(vector3: &flat::Vector3) -> Self {
         Self {
@@ -152,14 +84,6 @@ impl From<&flat::Vector3> for Vector3 {
     }
 }
 
-fn deserialize_vector3(vector3: &flat::Vector3) -> Vector3 {
-    Vector3 {
-        x: vector3.x(),
-        y: vector3.y(),
-        z: vector3.z(),
-    }
-}
-
 impl From<&flat::Rotator> for Rotator {
     fn from(rotator: &flat::Rotator) -> Self {
         Self {
@@ -167,14 +91,6 @@ impl From<&flat::Rotator> for Rotator {
             yaw: rotator.yaw(),
             roll: rotator.roll(),
         }
-    }
-}
-
-fn deserialize_rotator(rotator: &flat::Rotator) -> Rotator {
-    Rotator {
-        pitch: rotator.pitch(),
-        yaw: rotator.yaw(),
-        roll: rotator.roll(),
     }
 }
 
@@ -191,17 +107,6 @@ impl From<flat::Touch<'_>> for Touch {
     }
 }
 
-fn deserialize_touch(touch: flat::Touch<'_>) -> Touch {
-    Touch {
-        player_name: touch.playerName().unwrap().to_string(),
-        game_seconds: touch.gameSeconds(),
-        location: deserialize_vector3(touch.location().unwrap()),
-        normal: deserialize_vector3(touch.normal().unwrap()),
-        team: touch.team(),
-        _non_exhaustive: (),
-    }
-}
-
 impl From<flat::DropShotBallInfo<'_>> for DropshotBallInfo {
     fn from(info: flat::DropShotBallInfo<'_>) -> Self {
         Self {
@@ -210,15 +115,6 @@ impl From<flat::DropShotBallInfo<'_>> for DropshotBallInfo {
             force_accum_recent: info.forceAccumRecent(),
             _non_exhaustive: (),
         }
-    }
-}
-
-fn deserialize_dropshot_ball_info(info: flat::DropShotBallInfo<'_>) -> DropshotBallInfo {
-    DropshotBallInfo {
-        absorbed_force: info.absorbedForce(),
-        damage_index: info.damageIndex(),
-        force_accum_recent: info.forceAccumRecent(),
-        _non_exhaustive: (),
     }
 }
 
@@ -239,34 +135,12 @@ impl From<flat::GameInfo<'_>> for GameInfo {
     }
 }
 
-fn deserialize_game_info(info: flat::GameInfo<'_>) -> GameInfo {
-    GameInfo {
-        seconds_elapsed: info.secondsElapsed(),
-        game_time_remaining: info.gameTimeRemaining(),
-        is_overtime: info.isOvertime(),
-        is_unlimited_time: info.isUnlimitedTime(),
-        is_round_active: info.isRoundActive(),
-        is_kickoff_pause: info.isKickoffPause(),
-        is_match_ended: info.isMatchEnded(),
-        world_gravity_z: info.worldGravityZ(),
-        game_speed: info.gameSpeed(),
-        _non_exhaustive: (),
-    }
-}
-
 impl From<flat::DropshotTile<'_>> for DropshotTile {
     fn from(tile: flat::DropshotTile<'_>) -> Self {
         Self {
             tile_state: tile.tileState(),
             _non_exhaustive: (),
         }
-    }
-}
-
-fn deserialize_dropshot_tile(tile: flat::DropshotTile<'_>) -> DropshotTile {
-    DropshotTile {
-        tile_state: tile.tileState(),
-        _non_exhaustive: (),
     }
 }
 
@@ -277,14 +151,6 @@ impl From<flat::TeamInfo<'_>> for TeamInfo {
             score: info.score(),
             _non_exhaustive: (),
         }
-    }
-}
-
-fn deserialize_team_info(info: flat::TeamInfo<'_>) -> TeamInfo {
-    TeamInfo {
-        team_index: info.teamIndex(),
-        score: info.score(),
-        _non_exhaustive: (),
     }
 }
 
@@ -300,18 +166,5 @@ impl From<flat::ScoreInfo<'_>> for ScoreInfo {
             demolitions: info.demolitions(),
             _non_exhaustive: (),
         }
-    }
-}
-
-fn deserialize_score_info(info: flat::ScoreInfo<'_>) -> ScoreInfo {
-    ScoreInfo {
-        score: info.score(),
-        goals: info.goals(),
-        own_goals: info.ownGoals(),
-        assists: info.assists(),
-        saves: info.saves(),
-        shots: info.shots(),
-        demolitions: info.demolitions(),
-        _non_exhaustive: (),
     }
 }
