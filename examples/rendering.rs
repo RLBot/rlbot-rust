02137ba152296d97a35116068418290aaea099be
Nathan Caldwell <saintdev@gmail.com>
2020-03-04 22:07:12 -0700

    Fix rendering example

--- examples/rendering.rs
+++ examples/rendering.rs
@@ -6,6 +6,8 @@
 
 use std::{error::Error, f32::consts::PI};
 
+const RADIUS: f32 = 250.0;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let rlbot = rlbot::init()?;
 
@@ -25,35 +27,45 @@ fn main() -> Result<(), Box<dyn Error>> {
         total_ms -= sec * 1000;
         let min = total_ms / 1000 / 60;
 
-        let center_x = 100.0;
-        let center_y = 150.0;
+        let center_x = 0.0;
+        let center_y = -5120.0;
+        let center_z = 1540.0;
 
         let clock_hand = |fraction: f32, radius: f32| {
             let t = fraction * 2.0 * PI - PI / 2.0;
-            (center_x + t.cos() * radius, center_y + t.sin() * radius)
+            (
+                center_x + t.cos() * radius,
+                center_y,
+                center_z - t.sin() * radius,
+            )
         };
 
         let mut group = rlbot.begin_render_group(0);
         let green = group.color_rgb(0, 255, 0);
-        group.draw_string_2d(
-            (40.0, 20.0),
+        let text = format!("{}:{:02}.{:03}", min, sec, ms);
+        group.draw_string_3d(
+            (
+                center_x - 120.0 * text.len() as f32 / 2.0,
+                center_y,
+                center_z - RADIUS - 100.0,
+            ),
             (2, 2),
-            format!("{}:{:02}.{:03}", min, sec, ms),
+            text,
             green,
         );
-        group.draw_line_2d(
-            (center_x, center_y),
-            clock_hand(min as f32 / 60.0, 60.0),
+        group.draw_line_3d(
+            (center_x, center_y, center_z),
+            clock_hand(min as f32 / 60.0, RADIUS * 0.75),
             green,
         );
-        group.draw_line_2d(
-            (center_x, center_y),
-            clock_hand(sec as f32 / 60.0, 80.0),
+        group.draw_line_3d(
+            (center_x, center_y, center_z),
+            clock_hand(sec as f32 / 60.0, RADIUS),
             green,
         );
-        group.draw_line_2d(
-            (center_x, center_y),
-            clock_hand(ms as f32 / 1000.0, 40.0),
+        group.draw_line_3d(
+            (center_x, center_y, center_z),
+            clock_hand(ms as f32 / 1000.0, RADIUS / 2.0),
             green,
         );
         group.render()?;
